2245f6250c71d7233682df60f293b92384ebdfa4
riya <riyaparekh42@gmail.com>
2022-11-20 11:36:09 +0530

    commit for checktoken

--- jwt/checktoken.js
+++ jwt/checktoken.js
@@ -3,12 +3,13 @@ const localStorage = require('localStorage');
 
 const checktoken = (req, res, next) => {
   var auth = req.headers.authorization;
-  auth = auth.split(" ")[1];
-  console.log(auth);
+
   if (!auth) {
     res.status(401).send("Please authenticate using valid token");
   }
   else {
+    auth = auth.split(" ")[1];
+    console.log(auth);
     try {
       const data = jwt.verify(auth, process.env.JWT_SECRET);
       req.user = data.user;
